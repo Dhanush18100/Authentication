5ae2c8dfa6537f56a0e25c88ea4382e6f8989b24
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-09-30 09:25:06 +0530

    added register , login , logout routes

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import { register } from '../controllers/authController.js'
+import { login, logout } from '../controllers/authController.js';
+
+const authRouter=express.Router()
+
+authRouter.post('/register',register);
+authRouter.post('/login',login);
+authRouter.post('/logout',logout);
+
+
+
+export default authRouter;
