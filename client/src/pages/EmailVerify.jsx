eb31b3b01c898e0549983e81b682dffda9165d08
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-10-05 06:40:21 +0530

    verify email using otp

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { assets } from '../assets/assets'
 import axios from 'axios'
 import { useContext } from 'react'
@@ -58,6 +58,11 @@ const EmailVerify = () => {
     }
     
   }
+
+  useEffect(() => {
+    isLoggedin && userData && userData.isAccountVerified && navigate('/')
+  }, [isLoggedin,userData])
+  
   return (
     <div className='flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-purple-400'>
       <img onClick={()=>navigate('/')} src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' />
