019529bda47ff4e59571f148eec02009df150fd6
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-10-05 09:05:16 +0530

    connected password reset to backend api

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,11 +1,21 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContex'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const ResetPassword = () => {
+
+  const {backendUrl}=useContext(AppContext);
+  axios.defaults.withCredentials=true;
+
   const navigate = useNavigate();
   const [email,setEmail]=useState('')
   const [newPassword,setNewPassword]=useState('')
+  const [isEmailSent,setIsEmailSent]=useState('');
+  const [otp,setOtp]=useState(0);
+  const [isOtpSubmitted, setIsOtpSubmitted]=useState(false)
   const inputRefs = React.useRef([])
     const handleInput=(e,index)=>{
       if(e.target.value.length>0 && index < inputRefs.current.length-1){
@@ -29,13 +39,53 @@ const ResetPassword = () => {
   
       })
     }
+
+    const onSubmitEmail=async (e) => {
+      e.preventDefault();
+      try {
+        const {data}=await axios.post(backendUrl + '/api/auth/sent-reset-otp',{email})
+        data.sucess ? toast.success(data.message): toast.error(data.message)
+
+        data.sucess && setIsEmailSent(true)
+      } catch (error) {
+        toast.error(error.message)
+      }
+      
+    }
+
+    const onSubmitOTP=async (e) => {
+      e.preventDefault();
+      const otpArray= inputRefs.current.map(e=>e.value)
+      setOtp(otpArray.join(''))
+      setIsOtpSubmitted(true)
+      
+    }
+
+    const onSubmitNewPassword=async (e) => {
+      e.preventDefault();
+
+      try {
+        const {data}=await axios.post(backendUrl + '/api/auth/reset-password',{email,otp,newPassword})
+
+        data.sucess ? toast.success(data.message) : toast.error(data.message)
+
+        data.sucess && navigate('/login')
+
+      } catch (error) {
+        toast.error(error.message)
+        
+      }
+      
+    }
   
   return (
     <div className='flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-purple-400'>
       <img onClick={() => navigate('/')} src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' />
 
       {/* To enter email id */}
-      <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+
+      {!isEmailSent && 
+      <form onSubmit={onSubmitEmail} className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
         <h1 className='text-white text-2xl font-semibold text-center mb-4'>Reset Password</h1>
         <p className='text-center mb-6 text-indigo-300'>Enter your registered email address</p>
         <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
@@ -48,9 +98,12 @@ const ResetPassword = () => {
 
 
       </form>
+}
 
       {/* New form to enter otp */}
-      <form  className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+
+      {!isOtpSubmitted && isEmailSent &&
+      <form onSubmit={onSubmitOTP}  className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
         <h1 className='text-white text-2xl font-semibold text-center mb-4'>Reset Password OTP</h1>
         <p className='text-center mb-6 text-indigo-300'>Enter the 6-digit code sent to your email id</p>
 
@@ -68,9 +121,11 @@ const ResetPassword = () => {
 
 
       </form>
+}
 
       {/* Enter new Password */}
-      <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+      {isOtpSubmitted && isEmailSent &&
+      <form onSubmit={onSubmitNewPassword} className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
         <h1 className='text-white text-2xl font-semibold text-center mb-4'>New Password</h1>
         <p className='text-center mb-6 text-indigo-300'>Enter your new Password below</p>
         <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
@@ -83,6 +138,7 @@ const ResetPassword = () => {
 
 
       </form>
+}
 
     </div>
   )
